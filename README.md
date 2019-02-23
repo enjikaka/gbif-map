3812c5d6ed651912d3b17e8e900ec8924a73666d
Jeremy Karlsson <karlsson@jeremy.se>
2019-02-23 22:36:33 +0100

    Make map work.

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Note: Uses ResizeObserver. Polyfill it on your end.
 | `center-latitude` | Latitude value for the center point. Float32. |
 | `center-longitude` | Longitude value for the center point. Float32. |
 | `gbif-id` | The ID for the item on GBIF. Number. |
+| `gbif-style` | The style for the GBIF layer. See link in developer notes to API with all styles listed. |
 | `controls` | Wether or not to allow controls in the map. Boolean. |
 
 ## Usage
@@ -20,3 +21,7 @@ Note: Uses ResizeObserver. Polyfill it on your end.
 Install gbif-map via npm and import it in your webpack/rollup bundles.
 
 Alternatively, import it in your ES module supported browser with `import 'https://unpkg.com/gbif-map?module';` in a JavaScript module file or in your HTML with `<script src="https://unpkg.com/gbif-map?module" type="module"></script>`. The demo page uses the latter method.
+
+## Developer notes
+
+GBIF API is available at https://www.gbif.org/developer/maps
--- index.js
+++ index.js
@@ -1,8 +1,69 @@
+// @ts-ignore
+import * as L from 'https://unpkg.com/leaflet@1.4.0/dist/leaflet-src.esm.js';
+
 class GBIFMap extends HTMLElement {
   async connectedCallback () {
     this.sDOM = this.attachShadow({ mode: 'closed' });
 
-    this.sDOM.innerHTML = ``;
+    this.sDOM.innerHTML = `
+      <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
+      <style>
+      :host {
+        display: block;
+        width: 100%;
+        height: 100%;
+        contain: strict;
+        overflow: hidden;
+        position: relative;
+      }
+
+      #map {
+        width: 100%;
+        height: 100%;
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+
+      :host-context(.map--no-controls) #map {
+        pointer-events: none;
+      }
+
+      :host-context(.map--no-controls) .leaflet-control  {
+        display: none;
+      }
+
+      .leaflet-control-attribution {
+        display: none;
+      }
+      </style>
+      <div id="map"></div>
+    `;
+
+    const mapWrapperElement = this.sDOM.querySelector('#map');
+
+    const gbifId = this.getAttribute('gbif-id');
+    const gbifStyle = this.getAttribute('gbif-style') || 'purpleHeat.point';
+    const latitude = parseFloat(this.getAttribute('center-latitude'));
+    const longitude = parseFloat(this.getAttribute('center-longitude'));
+    const zoom = this.getAttribute('zoom') || 4;
+    const enableControls = this.hasAttribute('controls');
+
+    if (!enableControls) {
+      this.classList.add('map--no-controls');
+    }
+
+    const map = L.map(mapWrapperElement).setView([latitude, longitude], zoom);
+
+    const baseMap = L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
+      maxZoom: 18
+    });
+
+    const gbifOccurance = L.tileLayer(`https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?srs=EPSG:3857&style=${gbifStyle}&taxonKey=${gbifId}`);
+
+    map.addLayer(baseMap);
+    map.addLayer(gbifOccurance);
   }
 }
 
